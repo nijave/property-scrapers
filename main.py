09295b122d199d67ea28531d1279350759af0603
Nick Venenga <nijave@gmail.com>
2018-08-19 17:20:00 -0400

    Add AR County Data site and improve output formatting

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 # 1 Grabbing Excel file directly and parsing
-parcel_numbers = pd.read_excel("http://cosl.org/negexcel/BENTON.xls")['Parcel #']
+parcels = pd.read_excel("http://cosl.org/negexcel/BENTON.xls")['Parcel #']
 
 # (or) 2 Loading the website and scraping
-browser = mechanicalsoup.StatefulBrowser()
+browser = mechanicalsoup.StatefulBrowser(soup_config={'features': 'lxml'})
 browser.open("http://cosl.org/postauctionlist.aspx?ID=BENT&CTY=BENT&CN=BENTON")
 page = browser.get_current_page()
 table = page.find("table", {"id": "GridView1"})
@@ -22,14 +23,12 @@ for row in rows:
         headers = [ele.text.strip() for ele in row.find_all('th')]
         continue  # No valid cells in this row (header row?)
     text = [ele.text.strip() for ele in cols]  # Get texts from elements
-    parcel_list.append(text[5])
-
-print(parcel_numbers)
-print(parcel_list)
+    parcel_list.append(text[4])
 
 # Do searches using the data above on another website
 # This website tracks state between pages so you need to navigate between the
 # different pages before trying a search
+# This might not work for multiple pages of results
 browser = mechanicalsoup.StatefulBrowser()
 # Go here https://www.arcountydata.com/county.asp?county=Benton&s=R
 browser.open("https://www.arcountydata.com/county.asp?county=Benton&s=R")
@@ -38,4 +37,37 @@ browser.open("https://www.arcountydata.com/county.asp?county=Benton&s=R")
 browser.open("https://www.arcountydata.com/sponsored.asp")
 
 # Then fill out search_form
-page = browser.get_current_page()
+browser.select_form("#search_form")
+params = {"Subdivision": "",
+          "OBYIType": "",
+          "occupancyType": "",
+          "searchCounties": "",
+          "ParcelNumberModifier": "begins",
+          "ParcelNumber": "16-12473-000",
+          "OwnerModifier": "starts",
+          "OwnerName": "",
+          "StreetNumber": "",
+          "siteDirection": "",
+          "StreetName": "",
+          "City": "",
+          "ParcelParcelType": "",
+          "search": "Search"}
+
+for k, v in params.items():
+    browser[k] = v
+
+browser.submit_selected("search")
+table = browser.get_current_page().find("form", {"id": "parcel_report"}).find("table")
+# Convert beautifulsoup resultset of links to "linkName linkAddress" items
+parcel_links = ["%s %s" % (ele.text, ele.attrs["href"]) for ele in table.find_all('a')]
+
+print("""
+Parcels using Pandas:
+{parcels}
+
+Parcels using mechanicalsoup:
+{parcel_list}
+
+Parcel links:
+{parcel_links}
+""".format(parcels="\n".join(parcels.tolist()), parcel_list="\n".join(parcel_list), parcel_links="\n".join(parcel_links)))
